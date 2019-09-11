bcb3e0e2e58d49cab687209e3e7abcb917fb1d94
zhd407108459 <407108459@qq.com>
2019-09-12 04:25:58 +0800

    Project 1 4pm 9/12

---
+++ AI_Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.mousePosition.y > Screen.height * 0.95f && Input.mousePosition.y <= Screen.height)
+        {
+            transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z - moveSpeed * Time.deltaTime);
+        }
+        if (Input.mousePosition.y < Screen.height * 0.05f && Input.mousePosition.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z + moveSpeed * Time.deltaTime);
+        }
+        if (Input.mousePosition.x > Screen.width * 0.95f && Input.mousePosition.x <= Screen.width)
+        {
+            transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z + moveSpeed * Time.deltaTime);
+        }
+        if (Input.mousePosition.x < Screen.width * 0.05f && Input.mousePosition.x >= 0)
+        {
+            transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z - moveSpeed * Time.deltaTime);
+        }
+        if (transform.position.x > 65)
+        {
+            transform.position = new Vector3(65, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < 30)
+        {
+            transform.position = new Vector3(30, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > 80)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 80);
+        }
+        if (transform.position.z < 30)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 30);
+        }
+    }
+}
---
+++ AI_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public Vector3 respawnPosition;
+    public GameObject deathText;
+    void Start()
+    {
+        HideDeathText();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - Input.GetAxis("Vertical") * speed * Time.deltaTime, transform.position.y, transform.position.z - Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x - Input.GetAxis("Horizontal") * speed * Time.deltaTime, transform.position.y, transform.position.z + Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Water"))
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        transform.position = respawnPosition;
+        ShowDeathText();
+        Invoke("HideDeathText", 0.8f);
+    }
+
+    private void ShowDeathText()
+    {
+        deathText.SetActive(true);
+    }
+
+    private void HideDeathText()
+    {
+        deathText.SetActive(false);
+    }
+}
